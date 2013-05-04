22d926038125b088642bfe00b33295ca90dd8da4
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-05-05 01:07:24 +0600

    Added thread read expire cache when id changed

--- app/models/thread_sweeper.rb
+++ app/models/thread_sweeper.rb
@@ -7,7 +7,7 @@ class ThreadSweeper < ActionController::Caching::Sweeper
 
   private
   def expire_cache_for(thread_read)
-    Rails.cache.delete("total_unread_thread_by_#{thread_read.user_id}")
+    Rails.cache.delete("total_unread_thread_by_#{thread_read.user_id}") if thread_read.id_changed? or thread_read.read_changed?
   end
 
 end
