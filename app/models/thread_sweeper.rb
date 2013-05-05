6ecd0d576d19afd05900b828c23082a267993e56
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-05-05 22:02:54 +0600

    Fixed a bug for sweeping

--- app/models/thread_sweeper.rb
+++ app/models/thread_sweeper.rb
@@ -5,9 +5,13 @@ class ThreadSweeper < ActionController::Caching::Sweeper
     expire_cache_for(thread_read) if thread_read.read_changed?
   end
 
+  def after_create(thread_read)
+    expire_cache_for(thread_read)
+  end
+
   private
   def expire_cache_for(thread_read)
-    Rails.cache.delete("total_unread_thread_by_#{thread_read.user_id}") if thread_read.id_changed? or thread_read.read_changed?
+    Rails.cache.delete("total_unread_thread_by_#{thread_read.user_id}")
   end
 
 end
