5e4051406c52c551339e7ed488dfb9abae0808c4
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-05-25 10:42:42 +0600

    Now the helper is added to the main app

--- lib/discussion/engine.rb
+++ lib/discussion/engine.rb
@@ -1,5 +1,9 @@
 module Discussion
   class Engine < ::Rails::Engine
     isolate_namespace Discussion
+
+    config.after_initialize do
+      ActionView::Base.send(:include, Discussion::CommentsHelper)
+    end
   end
 end
